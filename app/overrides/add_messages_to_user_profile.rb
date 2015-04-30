259cba049281d0072565cea0b6e5574cde2e69ae
Moshe Holtzberg <moholtzberg@gmail.com>
2015-04-30 12:15:09 -0400

    fixed override

--- app/overrides/add_messages_to_user_profile.rb
+++ app/overrides/add_messages_to_user_profile.rb
@@ -1,6 +1,8 @@
 Deface::Override.new(
-  virtual_path: 'spree/users',
-  name: 'messages_user_profile',
-  replace_contents: '#my_messages',
-  partial: 'spree/shared/user_messages'
+  virtual_path: 'spree/users/show',
+  name: 'add_messages_to_user_profile',
+  replace_contents: 'div#my_messages',
+  partial: 'spree/shared/user_messages',
+  :original => '5eb8253cdf65f44314687972198c43eccc6b6d42',
+  disabled: false
 )
